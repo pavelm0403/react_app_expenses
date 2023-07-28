c884201750566c043097d08a299de77003c182fb
pavelm0403 <machenii_13@mail.ru>
2023-07-28 21:01:18 +0200

    28.07.2023 (3 section complete)

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -5,21 +5,13 @@ import React, { useState } from 'react';
  
 const CostItem = (props) => {
 
-  const [description, setDescription] = useState(props.description);
-
-
-  const changeDescriptionHandler = () => {
-    setDescription('New Coast');
-    console.log(description);
-  }
-
   return (
 
     <Card className='cost-item'>
       <CostDate date={props.date}/>
       <div className='cost-item__description'>
         <h2>{description}</h2>
-        <div classNa  me='cost-item__price'>${props.amount}</div> 
+        <div className='cost-item__price'>${props.amount}</div> 
       </div>
       <button onClick={changeDescriptionHandler} >change offs</button>
     </Card>
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,28 +1,38 @@
+import { useState } from "react";
+import CostsFilter from "../NewCost/CostsFilter";
 import Card from "../UI/Card";
 import CostItem from "./CostItem";
 import "./Costs.css";
 
-const Costs = (props) =>  {
+const Costs = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year)
+  };
 
   return (
-    <Card className="costs">
-      <CostItem 
-        date={props.costs[0].date}
-        description={props.costs[0].description}
-        amount={props.costs[0].amount}
-      />
-      <CostItem
-        date={props.costs[1].date}
-        description={props.costs[1].description}
-        amount={props.costs[1].amount}
-      />
-      <CostItem
-        date={props.costs[2].date}
-        description={props.costs[2].description}
-        amount={props.costs[2].amount}
-      />
-    </Card>
+    <div>
+      <Card className="costs">
+      <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostItem
+          date={props.costs[0].date}
+          description={props.costs[0].description}
+          amount={props.costs[0].amount}
+        />
+        <CostItem
+          date={props.costs[1].date}
+          description={props.costs[1].description}
+          amount={props.costs[1].amount}
+        />
+        <CostItem
+          date={props.costs[2].date}
+          description={props.costs[2].description}
+          amount={props.costs[2].amount}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Costs;
